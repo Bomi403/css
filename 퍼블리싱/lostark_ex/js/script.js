87a2d221a949efc1cd4064170d7534e8a9857cb8
Bomi403 <qhal2364@gmail.com>
2021-07-12 00:50:38 +0900

    화면 구현평가

---
+++ 퍼블리싱/lostark_ex/js/script.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+	var swiper = new Swiper(".main_banner .mySwiper", {
+		navigation: {
+			nextEl: ".main_banner .next",
+			prevEl: ".main_banner .prev",
+		},
+		pagination: {
+          el: ".main_banner .swiper-pagination",
+		  clickable : true,
+        },
+		loop : true,
+	});
+};
